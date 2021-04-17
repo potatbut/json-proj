f4a3d95deefbf33d69fc4e04626fd70fd7c0c2be
vera <darthyoon@gmail.com>
2021-04-17 10:09:47 +0300

    Babel update

--- webpack.config.js
+++ webpack.config.js
@@ -33,9 +33,14 @@ module.exports = {
           'sass-loader',
         ]
       },{
-        test: /\.js$/,
+        test: /\.m?js$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   }
